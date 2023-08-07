65cc7ce8ae3f82143c6f026921edc935a376e143
akkramm00 <127404065+akkramm00@users.noreply.github.com>
2023-08-07 18:57:25 +0000

    add calss HomeController

--- vendor/index.php
+++ vendor/index.php
@@ -63,12 +63,32 @@ $twig = new Twig_Environment($loader, [
     ?>
 
     <p>
-      La classe "Twig\Environment" esyt la classe principale de Twig. Elle est utilisdée pour créer une instance de l'environnement de Twig avec les configurations requises pour l'application.
+      La classe "Twig\Environment" est la classe principale de Twig. Elle est utilisdée pour créer une instance de l'environnement de Twig avec les configurations requises pour l'application.
       La classe "Twig\Loader\FilesystemLoader" est un hcargeurde modèles qui charge des fichiers de modèlesTwig à partir d'un repertoire sur le disque.
       Elle permet de spécifier le chemin d'accès aux modèles dans le système de fichiers et de charger les modèles nécéssaires.
  Lorsque nous céons un onbjet environnement Twig avec un chargeur de système de fichiers , le constructeur de la classe Twig\Environment prend en premier argument le chargeur de modèle que nous avons instancié.
-      
     </p>
+
+<?php
+class HomeController {
+  public function index() {
+    $loader = new Twig\Loader\FilesystemLoader('templates');
+    $twig = new Twig\Environment('$loader');
+    $template = $twig->load('form.twig');
+    echo $template->render([
+      'base_url' => BASE_URL?
+    ]);
+  }
+}
+?>
+<p>
+  Ce code est un exemple d'utilisation de Twig dans un controleur de page d'accueil.
+  dans la classe "HomeController", la methode  "index" est utilisée pour afficher la page d'accueil lorsque l'utilisateur y accède. Elle commence par instancier un objet"Twig\Loader\FilesystemLoader" qui charge les fichiers de modèle Twig à partir d'un dossierspécifié. Ensuite , un objet "Twig\Environment"
+est créé à partir de ce chargeur de système de fichiers. Le modèle "form.twig" est chargé en utilisnat la méthode "load" de l'objet "Twig\Environment" correspondant.
+Enfin la méthode "render" de cet objet est appelée pour générer le rendu final de la page au formay HTML. Les variables à passer au modèle Twig sont transmises sous forme de tableau associatif en argument de la méthode "render" 
+  Dans cet exemple , la variablr "BASE_URL" est transmise au modèle Twig sous le nom "base_url".
+  Enfin, le contenu est affichéà l'utilisateur en utilisant la commande "echo".
+</p>
     
   </body>
 </html>
